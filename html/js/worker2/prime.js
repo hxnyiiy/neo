41c7819b9de74fb9663c536d914be2f5caacd14f
hxnyiiy <a01051667732@gmail.com>
2025-04-07 17:34:57 +0900

    worker2 modify

--- html/js/worker2/prime.js
+++ html/js/worker2/prime.js
@@ -1,18 +1,17 @@
 function Prime(num) {
     if (num < 2) return false;
-    for (let i =2; i < num / 2; i++){
+    for (let i = 2; i < num / 2; i++) {
         if (num % i == 0) return false;
     }
     return true;
 }
 
-self.onmessage = function(e){
-    //work task로부터 전달받은 숫자
-    let input = e.data.input;
+self.onmessage = function(e) {
+    let number = e.data.input;
+
 
     output = number
-    output += (Prime(number)) ? "is PrimeNumber" : "is not PrimeNumber";
-    // 소수 판결 결과 전송
-    self.postMessage(output);
+    output += (Prime(number)) ? " is Primenumber" : " is not Primenumber";
+    postMessage(output);
 };
 
