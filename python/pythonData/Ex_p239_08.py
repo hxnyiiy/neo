b8391a2dd1822ca3067caa5d99c86b06838bd433
hxnyiiy <a01051667732@gmail.com>
2025-04-14 11:17:00 +0900

    python makinggraph

---
+++ python/pythonData/Ex_p239_08.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'NanumBarunGothic'
+
+filename = 'tips.csv'
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+filename = 'tips.csv' 
+
+myframe = pd.read_csv(filename, encoding='utf-8')
+print('head() 결제 총액')
+print(myframe.head())
+print('-' * 50)
+
+print('info() 팁 비용')
+print(myframe.info())
+print('-' * 50)
+
+mycolor = ['r', 'b']  
+labels = ['Female', 'Male']
+
+
+cnt = 0
+
+for finditem in labels:
+    
+    xdata = myframe.loc[myframe['sex'] == finditem, 'total_bill']
+    ydata = myframe.loc[myframe['sex'] == finditem, 'tip']
+
+    
+    plt.plot(xdata, ydata, label=finditem, color=mycolor[cnt], marker='o', linestyle='None')
+    cnt += 1
+
+
+plt.legend(loc=4) 
+plt.xlabel('결제 총액')
+plt.ylabel('팁 비용')
+plt.title('결제 총액과 팁 비용의 산점도')
+
+filename = 'Ex_p239_08.png'
+plt.savefig(filename, dpi=400, bbox_inches='tight')
+print(filename + ' saved')
+
+plt.show()
