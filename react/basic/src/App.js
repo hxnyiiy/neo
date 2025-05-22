919f0d930cffa6ee2eec3678cc61ac0494bb3d95
hxnyiiy <a01051667732@gmail.com>
2025-05-22 07:16:11 +0000

    react

---
+++ react/basic/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import InputSample from "./inputSample";
+
+function App() {
+  return <InputSample />;
+}
+
+export default App;
+
---
+++ react/basic/src/UserList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function User({ user }) {
+    <div>
+        <b>{user.username}</b>
+        <span>{user.email}</span>
+    </div>
+}
+
+function UserList() {
+    const users = [
+        {id:1, username:'developer', email: 'public.developer@gmail.com'},
+        {id:2, username:'tester', email: 'public.tester@gmail.com'},
+        {id:3, username:'moon', email: 'public.moon@gmail.com'},
+    ];
+    return (
+        <div>
+            {
+                users.map(user => (
+                    <User user={user} key={user.id} />
+                ))
+            }
+        </div>
+    )
+}
+
+export default UserList;
