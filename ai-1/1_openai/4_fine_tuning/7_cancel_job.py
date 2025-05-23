a73b74c6420133783074db6f3548d2be23e5bf69
hxnyiiy <a01051667732@gmail.com>
2025-05-23 08:59:30 +0000

    finetuning

---
+++ ai-1/1_openai/4_fine_tuning/7_cancel_job.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+client =OpenAI()
+
+fine_tunes = client.fine_tuning.jobs.list()
+
+for fine_tune in fine_tunes.data:
+    if fine_tunes.status == "running":
+        print(f'running fine-tuning job: {fine_tune.id}')
+        fine_tuning_cancel_job_id = fine_tune.id
+
+response = client.fine_tuning.jobs.cancel(fine_tuning_cancel_job_id)
+
+print(response)
+
