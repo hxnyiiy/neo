9604328604359b818c07bd1a8331a4ac19994470
hxnyiiy <a01051667732@gmail.com>
2025-05-28 08:10:04 +0000

    ai_openai

---
+++ ai-2/3_openai/p166_app.py
@@ -0,0 +1,25 @@
+import openai
+
+user_input = input("AI와 채팅할 내용을 입력하세요(종료하려면 end를 입력하세요. \n [나] : ")
+
+message = [{"role":"user", "content": user_input}]
+
+while True:
+    message = [
+        {"role": "assistant", "content": ai_message}
+        {"role": "user", "content": user_input}
+    ]
+    message.extend(message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    ai_message = response.choices[0].message.content
+    print(f'[AI] : {ai_message}' + '\n')
+
+    user_input = input("[나] : ")
+
+if(user_input.lower() == 'end'):
+    print("AI와 채팅을 종료합니다.")
